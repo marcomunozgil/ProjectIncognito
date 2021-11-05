4ca9a23485821ef947b439c7a91285c76a2fb2f4
MarcoMunozGil <ma.munozgil@gmail.com>
2021-11-04 17:21:16 -0700

    adding placard and report classes

---
+++ src/Placard.java
@@ -0,0 +1,31 @@
+public class Placard extends Report{
+
+    String placardColor = "no color yet"; //options are green, yellow and red
+
+    //method to choose the set the color of the placard on the provided report.
+    public String Placard (Report aReport) {
+
+        //variable for the color of the placard
+        String color;
+
+        //green is passing 85 or more...
+        if (reportScore > 85 || reportScore == 85) {
+            color = "green";
+            return color;
+        }
+
+        //yellow is temporary pass 70 to 84...
+        else if (reportScore < 85 && reportScore > 69) {
+            color = "yellow";
+            return color;
+        }
+        //otherwise under 69 or under is a failing score...
+        else {
+            color = "red";
+            return color;
+        }
+    }
+
+
+
+}
---
+++ src/Report.java
@@ -0,0 +1,30 @@
+
+
+public class Report {
+
+    //variables for the data of the report
+    String EstablishmentName = "no name";
+    String Address = " no address";
+    String date = " no date";
+    String ManagerName = " no name";
+    String inspector = " no inspector assigned";
+    int reportScore = 100;
+    String notes = "no notes";
+    boolean evidence = false; //meaning no pictures yet
+
+
+    //constructor for the report class
+    public Report () {
+        this.EstablishmentName = EstablishmentName;
+        this.Address = Address;
+        this.date = date;
+        this.ManagerName = ManagerName;
+        this.inspector = inspector;
+        this.reportScore = reportScore;
+        this.notes = notes;
+        this.evidence = evidence;
+    }
+
+    //need to add methods to edit the data and to filter the access
+    //to report data based on the user role.
+}
