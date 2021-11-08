f5a38353e342afa0b917ebbc54c4197fe032cc54
Azusena <senareyes258@gmail.com>
2021-11-07 19:35:43 -0800

    UI classes for dashboard

---
+++ src/dashboard/UIObj.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+
+public interface UIObject {
+
+
+    public void setValue(double d);
+
+    public double getValue();
+
+    public void setMax(double d);
+
+    public double getMax();
+
+    public void setMin(double d);
+
+    public double getMin();
+
+    public void draw(Graphics g);
+}
