11f786698d3346db7c99108ece5989f58fc88ec7
Jowyl Ong <jowylong@gmail.com>
2021-11-21 22:26:12 -0800

    Created ReportScore and ReportIterator for Iterator Pattern. Updated Establishment class so it can work within the package. Unit Test is WIP.

--- src/classes/Establishment.java
+++ src/classes/Establishment.java
@@ -1,3 +1,5 @@
+package classes;
+
 import java.util.*;
 public class Establishment {
     private String managerName;
---
+++ src/classes/ReportScore.java
@@ -0,0 +1,47 @@
+package classes;
+import inspections.*;
+
+
+import java.util.ArrayList;
+
+/**
+ * Lists the report scores in order for each establishment.
+ */
+public class ReportScore {
+
+    private ArrayList<Integer> score = new ArrayList<>();
+
+    public ReportIterator getReportIterator() {
+        return new ReportIterator() {
+            int index = 0;
+
+            @Override
+            public int next() {
+                int current = score.get(index);
+                index++;
+                return current;
+            }
+
+            @Override
+            public int previous() {
+                int current = score.get(index);
+                index--;
+                return current;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return index < score.size();
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                if (index >= 0) {
+                    return index < score.size();
+                }
+                return false;
+            }
+        };
+    }
+
+}
