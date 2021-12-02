8614f3793bd5d5f4f4764efc042295cf4ac3eb68
Jowyl Ong <jowylong@gmail.com>
2021-12-01 22:43:41 -0800

    Worked on log-in MVC and ReportScore classes.

--- src/model/LoginModel.java
+++ src/model/LoginModel.java
@@ -7,18 +7,34 @@ public class LoginModel {
     private String username;
     private char[] password;
 
+    /**
+     * Sets the username that the user typed
+     * @param username username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Sets the password that the user typed.
+     * @param password password
+     */
     public void setPassword(char[] password) {
         this.password = password;
     }
 
+    /**
+     * Gives the username.
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gives the password.
+     * @return password
+     */
     public char[] getPassword() {
         return password;
     }
